b3a207fb298ad2402ae041b4dba231c639905a1e
BlacuttMatias <matiasblacutt16@gmail.com>
2021-11-18 16:41:13 -0300

    Add ApiManager

---
+++ MiProjectoModular/Utlis/ApiManager.swift
@@ -0,0 +1,15 @@
+//
+//  ApiManager.swift
+//  MiProjectoModular
+//
+//  Created by Matias Blacutt on 18/11/2021.
+//
+
+import Foundation
+import SystemConfiguration
+
+struct ApiManager{
+    func checkConectivity() -> Bool{
+        return true
+    }
+}
