ce8449f791628e103be373240b18135f81cdd5fe
BlacuttMatias <matiasblacutt16@gmail.com>
2021-11-19 13:46:19 -0300

    Convert ApiManager in a Singleton and implement check connectivity

--- MiProjectoModular/Utlis/ApiManager.swift
+++ MiProjectoModular/Utlis/ApiManager.swift
@@ -7,9 +7,56 @@
 
 import Foundation
 import SystemConfiguration
+import Network
 
-struct ApiManager{
-    func checkConectivity() -> Bool{
-        return true
+class ApiManager{
+    
+    let baseURL = "https://jsonplaceholder.typicode.com/"
+    
+    private static var instance: ApiManager?
+    let musicEndPoint = "songs/"
+    var pathMonitor: NWPathMonitor
+    var path: NWPath?
+    
+    init(){
+        pathMonitor = NWPathMonitor()
+        pathMonitor.pathUpdateHandler = { Path in
+            self.path = Path
+        }
+        let elKiu = DispatchQueue (label: "NetworkMonitor")
+        pathMonitor.start(queue: elKiu)
+    }
+    
+    static func getInstance() -> ApiManager{
+        if(self.instance == nil) {
+            self.instance = ApiManager()
+        }
+        return self.instance!
+    }
+    
+    func getMusic(completion: @escaping ([Track]?, Error?) -> ()) {
+        let url : String = baseURL + self.musicEndPoint
+        let request : NSMutableURLRequest = NSMutableURLRequest(url: NSURL(string:  url)! as URL)
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) -> Void in
+            if error != nil {
+                completion(nil, error!)
+            }
+            else {
+                if let data = data {
+                    let result = try? JSONDecoder().decode([Track].self, from: data)
+                    completion (result, nil)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func checkConnectivity() -> Bool {
+        if let path = self.path {
+            return path.status == NWPath.Status.satisfied
+        }
+       return false
     }
 }
