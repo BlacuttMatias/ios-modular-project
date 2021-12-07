740ee28d381ec8b0ce88407ac126285ac43e293a
BlacuttMatias <matiasblacutt16@gmail.com>
2021-12-07 17:49:07 -0300

    Add timer to add hardcoded songs to table view

--- MiProjectoModular/ViewModels/TrackerViewModel.swift
+++ MiProjectoModular/ViewModels/TrackerViewModel.swift
@@ -14,6 +14,8 @@ class TrackerViewModel{
     private var loadTracksCallback: (([Track]?, Error?) -> ()) = {_,_ in }
     private weak var menuTrackerDelegate: MenuTrackerDelegate?
     var selectedCell: TrackTableViewCell?
+    var addElementsTimer: Timer?
+    let addElementsNotification: String = "addElementsNotification"
     
     init(apiManager: ApiManager, trackerDelegate: TrackerDelegate, menuTrackerDelegate: MenuTrackerDelegate){
         self.trackerDelegate = trackerDelegate
@@ -34,6 +36,13 @@ class TrackerViewModel{
             }
         }
         ApiManager.getInstance().getMusic(completion: loadTracksCallback)
+        self.addObserverToAddElements()
+        self.startTimerAddElements()
+    }
+    
+    deinit{
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(addElementsNotification), object: nil)
+        self.addElementsTimer?.invalidate()
     }
     
     func getNumberOfRows() -> Int{
@@ -96,6 +105,26 @@ class TrackerViewModel{
     func getMessageDeleteSongAction() -> String{
         let songTitle = selectedCell?.getTrack().title ?? "Unknown"
         return "The song \"\(songTitle)\" was removed from the Playlist"
+    }
+    
+    func startTimerAddElements(){
+        addElementsTimer = Timer.scheduledTimer(timeInterval: 30, target: self, selector: #selector(self.publishAddElementNotification), userInfo: nil, repeats: true)
+    }
+    
+    @objc func publishAddElementNotification(){
+        NotificationCenter.default.post(name: NSNotification.Name(addElementsNotification), object: nil)
+    }
+    
+    func addObserverToAddElements(){
+        NotificationCenter.default.addObserver(self, selector: #selector(self.addHardcodeElement), name: NSNotification.Name(addElementsNotification), object: nil)
+    }
+    
+    @objc func addHardcodeElement(){
+        let trackToAdd = Track(title: "cancion1", artist: "artista1", album: "album1", songId: "1010", genre: nil, duration: 146.42)
+        self.tracks.append(trackToAdd)
+        DispatchQueue.main.async {
+            self.trackerDelegate?.reloadDataTable()
+        }
     }
     
 }
